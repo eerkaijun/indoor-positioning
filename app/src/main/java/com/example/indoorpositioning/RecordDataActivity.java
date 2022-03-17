45188e867184093b316d5968740d37648f35ed94
Kai Jun Eer <kaijuneer@gmail.com>
2022-03-17 15:47:01 +0000

    set up RecordDataActivity

---
+++ app/src/main/java/com/example/indoorpositioning/RecordDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.indoorpositioning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_data);
+    }
+}
